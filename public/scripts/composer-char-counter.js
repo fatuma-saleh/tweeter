b71d935b91c1cc3d6eab22e46b534dd81da22609
Fatuma <sfatex@gmail.com>
2021-09-10 16:31:55 -0400

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  //console.log("callbackkkk")
+  $("#tweet-text").on('keyup', function() {
+    //console.log($(this).val().length); 
+    const characterLength = 140 - $(this).val().length;
+    console.log($(this).val().length);
+    let $form    = $(this).closest('form');
+    // console.log($form);
+    let $counter = $form.find('.counter')
+   // console.log($counter)
+    $counter.html(characterLength);
+    /* add a class named red  when counter < 0 */
+      
+    if (characterLength < 0){
+      $counter.addClass('red')
+    }
+  });
+
+});
+
+
