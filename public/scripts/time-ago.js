747b66dfd580d10149c1a93bf5abaa4ab87c3768
Fatuma <sfatex@gmail.com>
2021-09-12 17:16:59 -0400

    Add timeago functionality to the html & to the script

---
+++ public/scripts/time-ago.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+const $timeAgo = timeago.format(new Date()); 
+let $timeAgoComponent = $('article footer section.time-ago');
+$timeAgoComponent.text($timeAgo)
+
+
+});
