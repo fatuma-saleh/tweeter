f092d796235b03e7ae60c89d9a5062f8df80c163
Fatuma <sfatex@gmail.com>
2021-09-21 22:58:14 -0400

    Add code to avoid the previous tweets from been rendered everytime new tweet is posted and add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,35 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 $(document).ready(function () {
-$(".error").hide();
+
+  // fetching tweets with Ajax
+
+  const loadTweets = function () {
+    // const $form = $(this);
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    }).then(function (data) {
+      renderTweets(data);
+      // console.log(data)
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  };
+
   const renderTweets = function (tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
+
+    // avoid the previous tweets from been rendered everytime new tweet is posted
+
+    const $container = $("#tweets");
+    $container.empty();
+
+    // loops through tweets,calls createTweetElement for each tweet
+    // takes return value and prepends it to the tweets container
 
     for (let tweet of tweets) {
       const tweetElement = createTweetElement(tweet);
-      // $("#tweets").append(tweetElement)
       $("#tweets").prepend(tweetElement);
-
-
     }
   };
 
@@ -33,15 +46,13 @@ $(".error").hide();
     };
     const createdAt = newTime(tweet.created_at);
 
-    //Implementing Cross S.S
+    //Implementing Cross-Site Scripting
 
     const escape = function (str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
-
-    //const safeHTML = `<p>${escape(textFromUser)}</p>`;
 
     const $tweet = `
 <article class="tweet">
@@ -62,22 +73,18 @@ $(".error").hide();
           <i class="fas fa-flag"></i> 
           <i class="fas fa-retweet"></i>
           <i class="fas fa-heart"></i>
-          
           </section>
         </footer>
    </article>`;
 
-    // const $tweet = $(htmlTweet)
-    //console.log($tweet)
     return $tweet;
   };
-
 
   // form submission using jquery
 
   $("form").submit(function (event) {
     event.preventDefault();
-    //console.log(event)
+    // console.log(event)
     const $form = $(this);
 
     // validating the data before sending it to the server
@@ -90,7 +97,7 @@ $(".error").hide();
       $(".error").hide();
       const formData = $form.serialize();
       //const $input = $form.find('textarea');
-      //console.log('>>>>', $input.val())
+      // console.log('>>>>', $form)
       $.ajax({
         url: "/tweets",
         method: "POST",
@@ -99,7 +106,7 @@ $(".error").hide();
         //console.log('----data', data);
         loadTweets();
 
-        //Empty textarea after successful submission
+        //Empty textarea after successful submission and reset character counter back to 140
 
         $("#tweet-text").val("");
         $(".counter").text("140");
@@ -110,25 +117,11 @@ $(".error").hide();
 
   });
 
-  // fetching tweets with Ajax
 
-  const loadTweets = function () {
-    // const $form = $(this);
-    $.ajax({
-      url: "/tweets",
-      method: "GET",
-    }).then(function (data) {
-      renderTweets(data);
-    }).catch((error) => {
-      console.log(error);
-    });
+  // implementing the Go Up button
 
-  };
-// implementing the Go Up button
-
-let btn = $('#up-button');
-
-  $(window).scroll(function() {
+  let btn = $('#up-button');
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
       btn.addClass('show');
     } else {
@@ -136,15 +129,13 @@ let btn = $('#up-button');
     }
   });
 
-  btn.on('click', function(e) {
+  btn.on('click', function (e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    $('html, body').animate({ scrollTop: 0 }, '300');
   });
 
-
-
-
-
+  // Hide the error message and load the tweets when the form loads
+  $(".error").hide();
   loadTweets();
 
 
